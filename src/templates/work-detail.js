de06f5070e018012cfe03e4321bab59c3f997896
Rafael Cordones <rafael@cordones.me>
2017-12-10 23:05:21 +0100

    Wrok detail page: added <title> tag

--- src/templates/work-detail.js
+++ src/templates/work-detail.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Helmet from "react-helmet"
 import * as PropTypes from "prop-types"
 import Link from 'gatsby-link'
 
@@ -37,6 +38,7 @@ class WorkDetailTemplate extends React.Component {
         }
         return(
             <div className="work-detail col-xs-12 col-sm-12 col-lg-12" key={work.slug}>
+                <Helmet title={`Carmen Marcos Art - ${work.title}`} />
                 <img className="img-fluid rounded" src={ work.image.url }/>
                 <p className="text-left" id={work.slug}><em>{work.title}</em>. { workTechniqueAndMedium }. {work.height} cm. x {work.width} cm. {work.year}.</p>
                 <Link to="/">Back to homepage</Link>
